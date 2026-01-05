1982d893b354f06667155946a8daf55aec90c074
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-05 17:50:25 +0400

    Add performance to each called function

--- helpers/get_performance.ts
+++ helpers/get_performance.ts
@@ -1,4 +1,4 @@
-export default async function getPerformance(executedFunction: () => void) {
+export default async function getPerformance(executedFunction: () => Promise<void>) {
   const start = performance.now();
   await executedFunction();
   const end = performance.now();
