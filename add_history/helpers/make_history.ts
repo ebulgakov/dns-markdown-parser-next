a436c78bac4aa98384367d678f3289f42235e033
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-05 17:26:50 +0400

    Reduce requests to DB

---
+++ add_history/helpers/make_history.ts
@@ -0,0 +1,37 @@
+import { type PriceList } from "../../types/pricelist.ts";
+import { type History } from "../../types/history.ts";
+
+export default function makeHistory(priceLists: PriceList[]): History[] {
+  const history: History[] = [];
+  const flatCatalog = priceLists.map(pricelist =>
+    pricelist.positions.flatMap(position => position.items.flat())
+  );
+
+  const [lastCatalog, ...prevCatalogs] = flatCatalog;
+
+  for (let i = 0; i < lastCatalog.length; i++) {
+    const lastItem = lastCatalog[i];
+    const historyEntry: History = {
+      link: lastItem.link,
+      city: priceLists[0].city,
+      labels: [priceLists[0].createdAt],
+      priceOld: [lastItem.priceOld],
+      price: [lastItem.price],
+      profit: [lastItem.profit]
+    };
+
+    for (let j = 0; j < prevCatalogs.length; j++) {
+      const prevItem = prevCatalogs[j].find(item => item.link === lastItem.link);
+      if (prevItem) {
+        historyEntry.labels.unshift(priceLists[j + 1].createdAt);
+        historyEntry.priceOld.unshift(prevItem.priceOld);
+        historyEntry.price.unshift(prevItem.price);
+        historyEntry.profit.unshift(prevItem.profit);
+      }
+    }
+
+    history.push(historyEntry);
+  }
+
+  return history;
+}
--- add_history/index.ts
+++ add_history/index.ts
@@ -1,34 +1,17 @@
 import { dbConnect, dbDisconnect } from "../db/database.ts";
-import { getAllHistory } from "../db/history/queries/get_all_history.ts";
-import { getLastPriceList } from "../db/pricelist/queries/get_last_price_list.ts";
-import makeHistoryAndGetResult from "./helpers/make_history_and_get_result.ts";
-import { removeHistoryByLink } from "../db/history/mutations/remove_history_by_link.ts";
-import { updateHistoryByLink } from "../db/history/mutations/update_history_by_link.ts";
-import { addNewHistory } from "../db/history/mutations/add_new_history.ts";
+import { removeAllHistory } from "../db/history/mutations/remove_all_history.ts";
+import { getAllPriceLists } from "../db/pricelist/queries/get_all_price_lists.ts";
+import makeHistory from "./helpers/make_history.ts";
+import { addManyHistory } from "../db/history/mutations/add_many_history.ts";
 
 async function addHistory() {
   const city = process.env.CITY;
 
   await dbConnect();
-
-  const history = await getAllHistory(city);
-  const priceList = await getLastPriceList(city);
-  const { outdatedHistory, updatedHistory, newHistory } = makeHistoryAndGetResult(
-    priceList,
-    history
-  );
-
-  for (const record of outdatedHistory) {
-    await removeHistoryByLink(record.link);
-  }
-
-  for (const record of updatedHistory) {
-    await updateHistoryByLink(record.link, record);
-  }
-
-  for (const record of newHistory) {
-    await addNewHistory(record);
-  }
+  await removeAllHistory(city);
+  const priceLists = await getAllPriceLists(city);
+  const history = makeHistory(priceLists);
+  await addManyHistory(history);
 
   await dbDisconnect();
 }
