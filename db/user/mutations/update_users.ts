7fe79f2d515b22e8f4833df5f6bd53ac52422cbd
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-06 10:35:21 +0400

    Add TODO

--- db/user/mutations/update_users.ts
+++ db/user/mutations/update_users.ts
@@ -2,7 +2,7 @@ import { User } from "../../models/user_model.ts";
 import { type User as UserType } from "../../../types/user.ts";
 
 export const updateUsers = async (users: UserType[]) => {
-  if (users.length === 0) return;
+  // TODO: optimize with bulkWrite
   for (const user of users) {
     await User.updateOne({ _id: user._id }, { $set: user }, { upsert: true });
   }
