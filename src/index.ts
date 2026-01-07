9dfc6e46632a204cc83d95722faacb5bbb085715
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-07 09:48:36 +0400

    Fix port forwarding

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import server from "./server.js";
 
 // Start the server
-const port = process.env.PORT ?? "9001";
+const port = process.env.PORT ?? "3000";
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
