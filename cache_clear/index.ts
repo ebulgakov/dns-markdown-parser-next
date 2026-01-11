68ef3073e5760b1e36783fcc044bd445ffcb01cf
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-11 18:57:33 +0400

    Clear all cache after adding new price list

--- cache_clear/index.ts
+++ cache_clear/index.ts
@@ -2,7 +2,7 @@ import redis from "#cache/index.js";
 import getPerformance from "#helpers/get_performance.js";
 
 async function clearCache() {
-  const keys = await redis.keys(`pricelist:*`);
+  const keys = await redis.keys("*");
   console.log(`Clearing ${keys.length} cache keys related to pricelist...`);
   for (const key of keys) {
     await redis.del(key);
